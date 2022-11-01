2381807b23d17d3217f57c43c23242188d90a62f
alacrityj <alacritydanger@gmail.com>
2022-11-01 18:17:58 -0500

    initial commit

--- src/main/java/org/launchcode/hellospring/HelloSpringApplication.java
+++ src/main/java/org/launchcode/hellospring/HelloSpringApplication.java
@@ -2,7 +2,7 @@ package org.launchcode.hellospring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// initial commit
 @SpringBootApplication
 public class HelloSpringApplication {
 
