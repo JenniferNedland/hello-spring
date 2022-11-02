8257c2d8ba8b2483f02c01c47d57ccc442fd161a
alacrityj <alacritydanger@gmail.com>
2022-11-01 23:49:42 -0500

    added path-variables branch

--- src/main/java/org/launchcode/hellospring/HelloSpringApplication.java
+++ src/main/java/org/launchcode/hellospring/HelloSpringApplication.java
@@ -2,7 +2,7 @@ package org.launchcode.hellospring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// query parameters branch
+// path parameters branch
 @SpringBootApplication
 public class HelloSpringApplication {
 
