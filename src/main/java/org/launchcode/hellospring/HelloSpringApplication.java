248f5e36791f1be2078730d6062068913c909966
alacrityj <alacritydanger@gmail.com>
2022-11-03 23:49:28 -0500

    created controller-exercises branch

--- src/main/java/org/launchcode/hellospring/HelloSpringApplication.java
+++ src/main/java/org/launchcode/hellospring/HelloSpringApplication.java
@@ -2,7 +2,7 @@ package org.launchcode.hellospring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// class-annotations branch
+// controller-exercises branch
 @SpringBootApplication
 public class HelloSpringApplication {
 
